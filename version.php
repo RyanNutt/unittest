b176ec81be87cd85d754bca6e352cf97675014ea
Ryan Nutt <ryan@nutt.com>
2014-09-14 18:34:33 -0500

    updated version;

--- version.php
+++ version.php
@@ -14,10 +14,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_unittest';
-$plugin->version   = 2014080100;
+$plugin->version   = 2014091400;
 
 $plugin->requires  = 2012062500;
 
 $plugin->maturity  = MATURITY_STABLE;
 
-$plugin->release = '1.0.0';
+$plugin->release = '1.0.1';
